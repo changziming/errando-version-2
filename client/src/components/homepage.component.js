f2c984e14131cd18e803f53f9542f8a9fd65b4fd
ZAMC <changziming@users.noreply.github.com>
2020-07-01 21:48:04 +0800

    Errando v2, reorganised the frontend files to client

---
+++ client/src/components/homepage.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Homepage = () => (
+  
+  <body style= {{
+    margin: '0px',
+    padding: '25%',
+    height: 'fill',
+    width: 'fill',
+    textAlign: 'center',
+    backgroundColor: '#2f496e',
+    color: 'white'
+  }}>
+    <h1 style= {{
+      fontSize: '5rem'
+      }}>
+        errando
+      </h1>
+  </body>
+);
+
+export default Homepage;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+//require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI || 'mongodb://heroku_cqkqpp8h:pleudugi2ic3v5ijflntml0u65@ds113749.mlab.com:13749/heroku_cqkqpp8h'; // || 'mongodb+srv://Sean:9F!whuyvC6jZRdZ@cluster0-46pry.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const errandsRouter = require('./routes/errands');
+const usersRouter = require('./routes/users');
+
+app.use('/errands', errandsRouter);
+app.use('/users', usersRouter);
+
+// test code to deploy to heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
